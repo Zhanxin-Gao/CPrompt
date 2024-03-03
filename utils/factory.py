d29fc548121ad8849d23def4f4e82e668e97ba50
Zhanxin-Gao <92559222+Zhanxin-Gao@users.noreply.github.com>
2024-03-02 22:47:26 -0400

    Delete utils directory

--- utils/factory.py
+++
@@ -1,8 +0,0 @@
-from models.cprompt import CPrompt
-
-def get_model(model_name, args):
-    name = model_name.lower()
-    if name=="cprompt":
-        return CPrompt(args)
-    else:
-        assert 0
